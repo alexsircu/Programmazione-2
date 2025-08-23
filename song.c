840125502cd648a521c7ac2a52067dc901dddb84
Alexandru Sircu <alex.sircu@gmail.com>
2025-08-23 02:22:02 +0200

    Implement song exercise

---
+++ song.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define MAX 3
+#define MAXT 100
+#define MAXA 100
+
+int main(void) {
+    typedef struct {
+        char title[MAXT];
+        char author[MAXA];
+    } song;
+
+    song songs[MAX];
+
+    for (size_t i = 0; i < MAX; i++)
+    {
+        printf("Song title: ");
+        scanf(" %99[^\n]", songs[i].title);
+
+        printf("Song author: ");
+        scanf(" %99[^\n]", songs[i].author);
+    }
+
+    for (size_t i = 0; i < MAX; i++)
+    {
+        printf("Song title is: %s and the author is: %s\n", songs[i].title, songs[i].author);
+    }
+
+    return 0;
+}
